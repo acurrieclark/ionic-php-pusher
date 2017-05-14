96b93effabcef3a2c4daf11b6a43d2062e87c1c9
Alex Currie-Clark <acurrieclark@gmail.com>
2017-05-13 20:56:05 -0500

    Update PusherTest.php

--- tests/PusherTest.php
+++ tests/PusherTest.php
@@ -13,13 +13,13 @@ class PusherTest extends PHPUnit_Framework_TestCase {
   }
 
   public function testTokenSet() {
-    $token = API_TOKEN;
+    $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
     $pusher = new Pusher($token);
     $this->assertEquals($pusher->getAuthToken(), $token);
   }
 
   public function testTokenNotNull() {
-    $token = API_TOKEN;
+    $token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
     $pusher = new Pusher($token);
     $this->assertNotNull($pusher->getAuthToken());
     return $pusher;
